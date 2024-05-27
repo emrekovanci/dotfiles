27a7dcf4de78323c3a1f30c88fa2d311a7da13bd
Emre Kovanci <kovanciemre@gmail.com>
2024-05-27 09:49:12 +0300

    [nvim] add keymaps

--- nvim/lua/core/remap.lua
+++ nvim/lua/core/remap.lua
@@ -1,7 +1,13 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
+--
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
-vim.api.nvim_create_user_command("Q", "q", { })
+-- move up/down
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- search/replace
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
