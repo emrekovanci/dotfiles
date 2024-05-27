291a381419840cd44beedf8eda0b856475be69c7
Emre Kovanci <kovanciemre@gmail.com>
2024-05-27 09:59:18 +0300

    [windows] update powershell profile

--- config/Microsoft.PowerShell_profile.ps1
+++ config/Microsoft.PowerShell_profile.ps1
@@ -38,54 +38,18 @@ Set-PSReadLineOption @PSReadLineOptions
 # https://github.com/ajeetdsouza/zoxide
 Invoke-Expression (& { (zoxide init powershell | Out-String) })
 
-# Functions
 function whereis($command)
 {
     Get-Command -Name $command -ErrorAction SilentlyContinue |
     Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
 }
 
-# Ps-readline view toggle
-function set-predictionview
+function sview()
 {
-    [cmdletbinding(SupportsShouldProcess)]
-    [alias("spv", "sview")]
-    [OutputType("none")]
-    Param(
-        [Parameter(Position = 0, HelpMessage = "Specify the prediction style. Toggle will switch to the unused style.")]
-        [ValidateSet("List", "InLine", "Toggle")]
-        [string]$View = "Toggle",
-        [switch]$Passthru
-    )
-
-    Try
-    {
-        Switch ($View)
-        {
-            "List" { $style = "ListView" }
-            "Inline" { $style = "InLineView" }
-            "Toggle"
-            {
-                Switch ((Get-PSReadLineOption).PredictionViewStyle) 
-                {
-                    "InLineview" { $style = "ListView" }
-                    "ListView" { $style = "InLineView" }
-                    Default { Write-Warning "Could not determine a view style. Are you running PSReadline v2.2.2 or later?" }
-                }
-            }
-        }
-
-        if ($style -AND ($PSCmdlet.ShouldProcess($style, "Set prediction view style")))
-        {
-            Set-PSReadLineOption -PredictionViewStyle $style
-            if ($Passthru)
-            {
-                Get-PredictionView
-            }
-        }
-    }
-    Catch
-    {
-        Write-Warning "There was a problem. Could not determine a view style. Are you running PSReadline v2.2.2 or later? $($_.exception.message)."
+    $currentStyle = (Get-PSReadLineOption).PredictionViewStyle
+    if ($currentStyle -eq "InlineView") {
+        Set-PSReadLineOption -PredictionViewStyle ListView
+    } else {
+        Set-PSReadLineOption -PredictionViewStyle InlineView
     }
 }
