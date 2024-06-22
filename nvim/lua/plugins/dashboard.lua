e473d2131ddd756e362d22283796022606ba50d5
Emre Kovanci <kovanciemre@gmail.com>
2024-06-23 00:51:26 +0300

    [nvim] lualine bug fix

--- nvim/lua/plugins/dashboard.lua
+++ nvim/lua/plugins/dashboard.lua
@@ -17,6 +17,11 @@ return {
 
         local opts = {
             theme = "doom",
+            hide = {
+                -- this is taken care of by lualine
+                -- enabling this messes up the actual laststatus setting after loading a file
+                statusline = false,
+            },
             config = {
                 header = vim.split(logo, "\n"),
                 center = {
