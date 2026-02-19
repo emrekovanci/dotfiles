ab8efdcd31d90874fed80e0ff3d1b7810f3b2800
Emre Kovanci <kovanciemre@gmail.com>
2026-02-19 07:40:26 +0300

    Update init.el

--- emacs/init.el
+++ emacs/init.el
@@ -186,7 +186,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "CommitMono" :foundry "outline" :slant normal :weight regular :height 159 :width normal))))
+ '(default ((t (:inherit nil :extend nil :stipple nil :background "#181818" :foreground "#d1c7c5" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight regular :height 159 :width normal :foundry "outline" :family "CommitMono"))))
  '(icomplete-first-match ((t (:foreground "goldenrod" :weight bold))))
  '(link ((t (:foreground "#73c936" :underline t))))
  '(markdown-code-face ((t (:inherit ##)))))
