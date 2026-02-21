3c7b36a252ab22e4485a2cc1e016e54a720dead5
Emre Kovanci <kovanciemre@gmail.com>
2026-02-21 18:10:48 +0300

    Update deploy.ps1

--- deploy.ps1
+++ deploy.ps1
@@ -28,7 +28,7 @@ function New-Symlink {
 # Env Vars
 [System.Environment]::SetEnvironmentVariable("XDG_CONFIG_HOME",     "$HOME\Documents\GitHub\dotfiles\",                     "USER")
 [System.Environment]::SetEnvironmentVariable("STARSHIP_CONFIG",     "$HOME\Documents\GitHub\dotfiles\config\starship.toml", "USER")
-[System.Environment]::SetEnvironmentVariable("RIPGREP_CONFIG_PATH", "$HOME\Documents\GitHub\dotfiles\config\.ripgreprc",    "USER")
+#[System.Environment]::SetEnvironmentVariable("RIPGREP_CONFIG_PATH", "$HOME\Documents\GitHub\dotfiles\config\.ripgreprc",    "USER")
 
 # Symlinks
 New-Symlink `
