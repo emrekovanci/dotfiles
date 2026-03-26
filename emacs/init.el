9f95125a5b3885603fcf5bc1267a8a1b991e7001
Emre Kovanci <kovanciemre@gmail.com>
2026-03-26 16:14:42 +0300

    sync

--- emacs/init.el
+++ emacs/init.el
@@ -58,14 +58,11 @@
 
 ;; c / c++ things
 (defun my-c-mode-hook ()
-  (setq c-basic-offset 4)
-  (c-set-offset 'substatement-open 0)
-  (c-set-offset 'brace-list-intro 0)
-  (c-set-offset 'brace-list-entry 0)
-  (c-set-offset 'case-label 4))
+  (setq c-default-style "k&r"
+        c-basic-offset 4)
+  (c-set-offset 'substatement-open 0))
 
-(add-hook 'c++-mode-hook #'my-c-mode-hook)
-(add-hook 'c-mode-hook #'my-c-mode-hook)
+(add-hook 'c-mode-common-hook #'my-c-mode-hook)
 
 (add-to-list 'auto-mode-alist '("\\.clangd\\'" . yaml-mode))
 (add-to-list 'auto-mode-alist '("\\.clang-tidy\\'" . yaml-mode))
