4dde73d4b321d143a20f5480b24938488ed2e21b
Emre Kovanci <kovanciemre@gmail.com>
2026-04-04 12:56:23 +0300

    [nvim] rm unnec

--- nvim/lua/core/autocmds.lua
+++ nvim/lua/core/autocmds.lua
@@ -1,23 +1,4 @@
--- Show cursor line only in active window
-vim.api.nvim_create_autocmd({ "InsertLeave", "WinEnter" }, {
-    callback = function()
-        if vim.w.auto_cursorline then
-            vim.wo.cursorline = true
-            vim.w.auto_cursorline = nil
-        end
-    end,
-})
-
-vim.api.nvim_create_autocmd({ "InsertEnter", "WinLeave" }, {
-    callback = function()
-        if vim.wo.cursorline then
-            vim.w.auto_cursorline = true
-            vim.wo.cursorline = false
-        end
-    end,
-})
-
--- Highlight on yank
+-- highlight on yank
 vim.api.nvim_create_autocmd("TextYankPost", {
     group = vim.api.nvim_create_augroup("highlight_yank", {}),
     desc = "Hightlight selection on yank",
@@ -27,7 +8,7 @@ vim.api.nvim_create_autocmd("TextYankPost", {
     end,
 })
 
--- Quickfix
+-- quickfix
 vim.api.nvim_create_autocmd("FileType", {
     pattern = "qf",
     callback = function()
