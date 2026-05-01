a81fcae8fd8893ea5d4acf0a3531eefed872c681
Emre Kovanci <kovanciemre@gmail.com>
2026-05-01 23:19:27 +0300

    sync

--- emacs/init.el
+++ emacs/init.el
@@ -142,8 +142,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Rec Mono Casual" :foundry "outline" :slant normal :weight regular :height 142 :width normal))))
- '(font-lock-comment-face ((t (:foreground "forest green" :slant italic :weight bold))))
+ '(default ((t (:family "LMMono8" :foundry "outline" :slant normal :weight regular :height 160 :width normal))))
  '(font-lock-string-face ((t (:foreground "grey30" :slant normal))))
  '(markdown-code-face ((t (:inherit ##))))
  '(success ((t (:foreground "forest green" :weight bold))))
