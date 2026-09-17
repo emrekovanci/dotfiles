5b08976e8835ea740b0a986b9bd2521aaa37a459
Emre Kovanci <kovanciemre@gmail.com>
2026-09-17 18:32:15 +0300

    emacs - revert

--- emacs/init.el
+++ emacs/init.el
@@ -74,7 +74,7 @@
            (if-let ((project (project-current)))
                (project-root project)
              default-directory)))
-      (shell-command (format "wt.exe -w 0 -d \"%s\" lazygit" (expand-file-name default-directory))))))
+      (shell-command (concat "wt.exe -w 0 -d \"" (expand-file-name default-directory) "\" lazygit")))))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
